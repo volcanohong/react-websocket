feb56ec6076180d48627320d25aa90de4d49c7f8
volcanohong <volcanohong@gmail.com>
2019-10-23 16:40:35 -0700

    init ui

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div id="container">
-      <div id="content">
-        <fieldset>
-          <legend>URL</legend>
-          <div>
-              <input type="text" id="serverUrl" value="ws://example.com/websocket" width="500px" />
-              <button id="connectButton">Open</button>
-              <button id="disconnectButton">Close</button>
-          </div>
-          <div>
-              <label>Status:</label>
-              <span id="connectionStatus">CLOSED</span>
-          </div>
-        </fieldset>
-        <fieldset id="requestArea">
-          <legend>Request</legend>
-          <div>
-              <textarea id="sendMessage" disabled="disabled"></textarea>
-          </div>
-          <div>
-              <button id="sendButton" disabled="disabled">Send</button>
-              [Ctrl + Enter]
-          </div>
-        </fieldset>
-        <fieldset id="messageArea">
-          <legend>Message Log
-              <button id="clearMessage">Clear</button>
-          </legend>
-          <div id="messages"></div>
-        </fieldset>
+class App extends Component { 
+  constructor(props) {
+    console.log('app init...');
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <div id="content">
+          <fieldset>
+            <legend>URL</legend>
+            <div>
+                <input type="text" id="serverUrl" value="ws://example.com/websocket" width="500px" />
+                <button id="connectButton">Open</button>
+                <button id="disconnectButton">Close</button>
+            </div>
+            <div>
+                <label>Status:</label>
+                <span id="connectionStatus">CLOSED</span>
+            </div>
+          </fieldset>
+          <fieldset id="requestArea">
+            <legend>Request</legend>
+            <div>
+                <textarea id="sendMessage" disabled="disabled"></textarea>
+            </div>
+            <div>
+                <button id="sendButton" disabled="disabled">Send</button>
+                [Ctrl + Enter]
+            </div>
+          </fieldset>
+          <fieldset id="messageArea">
+            <legend>Message Log
+                <button id="clearMessage">Clear</button>
+            </legend>
+            <div id="messages"></div>
+          </fieldset>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
